0cc26f6ac8bc1a09449855e76eee6e321a375412
lemonsh <lemon@lemonsh.moe>
2023-03-24 00:09:48 +0100

    add cli

---
+++ src/cli.rs
@@ -0,0 +1,32 @@
+use clap::{Parser, Subcommand};
+
+/// Upload chunky files to Discord with webhooks
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+pub struct Args {
+    /// Subcommand
+    #[clap(subcommand)]
+    pub command: Command
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    /// Upload a file
+    Upload {
+        file: String,
+        #[clap(short)]
+        webhook: Option<String>,
+    },
+    /// Download a file
+    Download {
+        url: String,
+        #[clap(short)]
+        output: Option<String>
+    },
+    /// Delete a file
+    Delete {
+        file: String,
+        #[clap(short)]
+        webhook: Option<String>,
+    }
+}
